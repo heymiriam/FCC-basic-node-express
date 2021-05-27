76d7f5d023a2872d1bae84a884af56efc3896566
heymiriam <miriambellon@gmail.com>
2021-05-27 09:26:34 -0700

    ex. 5

--- myApp.js
+++ myApp.js
@@ -1,5 +1,43 @@
 var express = require('express');
 var app = express();
+const mySecret = process.env['MESSAGE_STYLE'];
+require('dotenv').config()
+console.log("Hello World");
+
+app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/json", function(req, res){
+  res.json({"message": "Hello json"})
+});
+
+app.get("/json", (req, res) => {
+     if(process.env.MESSAGE_STYLE === "uppercase"){
+       res.json({"message" : "HELLO JSON"});
+     }
+     res.json({"message" : "Hello json"});
+   });
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/", function(req, res) {
+res.send('Hello Express');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
